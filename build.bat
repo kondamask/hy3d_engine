f240ea774d0f7e461e6be70b5eaa4bb21435e8c5
heyyod <kondamask@gmail.com>
2022-07-26 22:54:58 +0300

    small changes in build script

--- build.bat
+++ build.bat
@@ -1,63 +1,99 @@
 @echo off
 
-ECHO ----------------------------------------------------------------------------
+ECHO -------------------------------------------------------------------------------------
+
 ctime -begin hy3d_engine_build_time.ctm
 
-REM Check if build mode is specified
+@REM -------------------------------------------------------------------------------------
+
+SET AppName=hy3d_engine
+
+@REM Common Flags ------------------------------------------------------------------------
+
+SET CompilerFlags=-EHsc -FC -fp:except- -fp:fast -Gm- -GS- -nologo -Oi -WL %ModeFlags%
+SET Defines=-D_CRT_SECURE_NO_WARNINGS  -DHY3D_EXPORT
+SET Includes=
+SET Warnings=-W4 -wd4100 -wd4458 -wd4505 -wd4201
+
+SET LinkerFlags=-incremental:no -opt:ref
+SET LIBs=user32.lib gdi32.lib
+SET OBJs=all.obj
+
+@REM Build Mode --------------------------------------------------------------------------
+
 IF [%1] == [] (
-	echo Enter build mode: Debug or Release
-	echo Example: build Debug
-	goto :EOF
+	ECHO Enter build mode: Debug or Release
+	ECHO Example: build Debug
+	GOTO :EOF
 )
 
-REM Set debug build options
 IF %1 == Debug (
 	SET ModeFlags=-MTd -Od -Zi -Zo
     SET OutputPath=.\bin\Debug
-	ECHO Building Debug...
+	ECHO Debug Mode
 )
-REM Set release build options
+
 IF %1 == Release (
 	SET ModeFlags=-O2
     SET OutputPath=.\bin\Release
-	ECHO Building Release...
+	ECHO Release Mode
+	COLOR 0A
 )
+
 IF NOT EXIST %OutputPath% MKDIR %OutputPath%
 PUSHD %OutputPath%
+SET SRC=..\..\src
+SET Includes=/I%SRC% %Includes%
 
-SET SRC=.\..\..\src
+SET CommonCompilerOptions=%CompilerFlags% %Defines% %Includes% %Warnings%
+SET CommonLinkerOptions=%LinkerFlags% %LIBs% %OBJs%
 
-REM COMPILER OPTIONS
-SET CFlags=-EHsc -FC -fp:except- -fp:fast -Gm- -GS- -nologo -Oi -WL %ModeFlags%
-SET Defines=-D_CRT_SECURE_NO_WARNINGS -DHY3D_EXPORT
-SET Includes=/I %SRC%
-SET Warnings=-W4 -wd4100 -wd4458 -wd4505 -wd4201
-SET AppName=hy3d_engine
-SET COptions=%CFlags% %Defines% %Includes% %Warnings% %MODE%
+@REM Build Targets -----------------------------------------------------------------------
 
-REM LINKER OPTIONS
-SET LFlags=-incremental:no -opt:ref
-SET Libraries=user32.lib gdi32.lib core.lib win32_platform.lib
+IF [%2] == [] (
+	ECHO Building All...
+	GOTO :BUILD_ALL
+)
+
+IF %2 == Engine (
+	ECHO Building Engine...
+	GOTO :BUILD_ENGINE
+)
+
+@REM -------------------------------------------------------------------------------------
+:BUILD_ALL
 
 DEL *.pdb > NUL 2> NUL
 
-REM Core
-cl /c %COptions% %SRC%\core\core.cpp
-lib /nologo core.obj
+@REM All Cpps ----------------------------------------------------------------------------
+cl	/c ^
+	%CommonCompilerOptions% ^
+	%SRC%\all.cpp
 
-REM Windows Platform
-REM rc /fo win32_hy3d.res /nologo %SRC%\platform\win32\resource.rc
-cl /c %COptions% %SRC%\platform\win32\win32_platform.cpp
-lib /nologo win32_platform.obj 
+@REM Windows Resource --------------------------------------------------------------------
+rc	/nologo ^
+	/fo win32_hy3d.res ^
+	%SRC%\platform\win32\resource.rc
 
-REM Engine
-cl %COptions% %SRC%\engine\engine.cpp -LD -link %LFlags% %Libraries% -PDB:engine_%RANDOM%.pdb
+@REM App Entry  --------------------------------------------------------------------------
+cl	%CommonCompilerOptions% ^
+	%SRC%\main.cpp win32_hy3d.res ^
+	-Fe%AppName% ^
+	-link ^
+	%CommonLinkerOptions%
 
-REM Main Application
-cl %COptions% %SRC%\main.cpp win32_hy3d.res -Fe%AppName% -link %LFlags% %Libraries%
+:BUILD_ENGINE
+@REM Engine
+cl	-LD ^
+	%CommonCompilerOptions% ^
+	%SRC%\engine\engine.cpp ^
+	-link ^
+	%CommonLinkerOptions% ^
+	-PDB:engine_%RANDOM%.pdb
+
 POPD
 
-ECHO ----------------------------------------------------------------------------
+ECHO -------------------------------------------------------------------------------------
 
 :EOF
 
---
+++ src/all.cpp
@@ -0,0 +1,2 @@
+#include "core/core.cpp"
+#include "platform/win32/win32_platform.cpp"
